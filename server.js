8a607b956c881e02582ac849eda5f156178c61cd
howdoicodeplease <saeidsaeity@outlook.com>
2024-12-05 12:22:41 +0000

    Auto-commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ console.log("Stremio Addon is starting");
 const addonInterface = require("./addon");
 
 // Serve the add-on on port 7000
-serveHTTP(addonInterface, { port: 7000 });
+const port = process.env.PORT || 7000;
+serveHTTP(addonInterface, { port });
 
 console.log("HTTP addon accessible at: http://127.0.0.1:7000/manifest.json");
