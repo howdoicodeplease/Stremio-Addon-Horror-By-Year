22a3c664dd5955ff9c3fa59b7ed5ba2d0254b049
howdoicodeplease <sdodie1@yahoo.co.uk>
2024-12-05 15:38:04 +0000

    adds run server

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ console.log("Stremio Addon is starting");
 // Import the addon interface
 const addonInterface = require("./addon");
 
-// Serve the add-on on port 7000
-const port = process.env.PORT
+// Get the port from the environment variable or use a default
+const port = process.env.PORT || 7000;
 
+// Serve the add-on
 serveHTTP(addonInterface, { port });
 
-console.log("HTTP addon accessible at: http://127.0.0.1:7000/manifest.json");
+console.log(`Stremio Addon is running on port ${port}`);
